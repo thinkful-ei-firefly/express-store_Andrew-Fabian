c9961df7398299c1e3fb72f50b29d44d876cd683
Fabian Lema <fabianlemac@gmail.com>
2019-08-22 14:11:42 -0500

    User path

--- src/app.js
+++ src/app.js
@@ -30,6 +30,63 @@ app.post('/', (req, res) => {
     .send('POST request received.');
 })
 
+app.post('/user', (req, res) => {
+  const {username, password, favoriteClub, newsLetter=false } = req.body;
+  if (!username){
+    return res
+      .status(400)
+      .send('Username required');
+  }
+
+  if(!password){
+    return res
+      .status(400)
+      .send('Password required')
+  }
+
+  if(!favoriteClub){
+    return res
+      .status(400)
+      .send('Favorite Club required')
+  }
+
+  if (username.length < 6 || username.length > 20){
+    return res
+      .status(401)
+      .send('Username must be between 6 and 20 characters');
+  }
+
+  if (password.length < 8 || password.length > 36){
+    return res
+      .status(401)
+      .send('Password must be between 8 and 36 characters');
+  }
+
+  if (!password.match(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/)) {
+    return res
+      .status(401)
+      .send('Password must be contain at least one digit');
+  }
+
+  const clubs = [
+    'Cache Valley Stone Society',
+    'Ogden Curling Club',
+    'Park City Curling Club',
+    'Salt City Curling Club',
+    'Utah Olympic Oval Curling Club'
+  ];
+
+  if (!clubs.includes(favoriteClub)){
+    return res
+      .status(402)
+      .send('Favorite Club must be one of: ' + clubs.join(', '))
+  }
+
+  res
+    .send('All validation passed.');
+
+})
+
 app.use(function errorHandler(error, req, res, next) {
   let response
   if (NODE_ENV === 'production') {
